70c8b3b4cfa3f5a178bf27e3ce1a9dd041f5821a
iceuserman <iceuserman@gmail.com>
2023-11-02 17:43:38 +0900

    메모수정

--- memo.cs
+++ memo.cs
@@ -30,7 +30,7 @@ SceneLoader CanvasGroup Alpha값 조절로 변경
 Dialog 스킵할때 말소리 한번에 나오는 것 수정
 
 -Addressable에 폰트 추가
-TMP_Settings.cs 설정파일 불러오기
+UITitle TMP_FontAsset 불러오는거까지 완료
 
 -일반노트 오브젝트 재활용
 Initialize시 모든 노트의 NoteDropper 기준 스폰 위치를 기록
@@ -76,10 +76,28 @@ Estate Grand LE for sforzando
 LithosPro 사용글자
 0123456789.+-JUDGEMS
 
+NanumSquareNeo 사용글자
+0123456789KMGB().?!필수데이터다운로드가필요합니다약지금다운로드하시겠습니까취소를누르면게임이종료됩니다중완료 
+
 영어     32-126     (95자)
 상용한글 44032-55203(11172자)
 한글자모 12593-12643(51자)
 특수문자 8200-9900  (1701자)
 
 
-TMP_Settings.s_Instance = Addressables.LoadAssetAsync<TMP_Settings>("TMP Settings").WaitForCompletion();
+tempFont = Addressables.LoadAssetAsync<TMP_FontAsset>(TMP_Settings.defaultFontAssetPath + new string(m_htmlTag, m_xmlAttribute[0].valueStartIndex, m_xmlAttribute[0].valueLength)).WaitForCompletion();
+
+
+	var fontHandle = Addressables.LoadResourceLocationsAsync("Font", typeof(ScriptableObject));
+        fontHandle.Completed += fontLocations =>
+        {
+            foreach (IResourceLocation location in fontLocations.Result)
+            {
+                string locationPrimaryKey = location.PrimaryKey;
+                Addressables.LoadAssetAsync<TMP_FontAsset>(locationPrimaryKey).Completed +=
+                    font =>
+                    {
+                        MaterialReferenceManager.AddFontAsset(font.Result);
+                    };
+            }
+        };
